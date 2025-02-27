111b43c262517708701832e5e7448f9b0ca39415
mayiii14 <mayflorpajarillo@gmail.com>
2025-02-27 22:55:01 +0800

    CS1C_MFPajarillo_C1-A1.js

--- CS1C_MFPajarillo_C1-A1.js
+++ CS1C_MFPajarillo_C1-A1.js
@@ -1,11 +1,10 @@
 //C1-A1: Variable/Constants Declaration and String Concatenation Methods
 
-
 // Student's Information
-let myname = "May Flor Pajarillo";
-let birthdate = "August 14, 2006";
-let birthplace = "Gabriela Silang General Hospital- Vigan City, Ilocos Sur, Philippines 2700";
-let address = "Manaboc, Burgos, Ilocos Sur, Philippines 2724";
+let myname = "Rizalyn Donato Domitita";
+let birthdate = "November 13, 2005";
+let birthplace = "Southern Ilocos Sur Dist. Hospital- Tagudin, Ilocos Sur, Philippines 2714";
+let address = "Al-Aludig, Santiago, Ilocos Sur, Philippines 2707";
 let course = "Bachelor of Science in Computer Science";
 let dreamJob = "Software Engineer";
 
@@ -17,10 +16,10 @@ let address_uppercase = address.toUpperCase();
 let course_lowercase = course.toLowerCase();
 
 // Classmate 1's Information
-let classmate1Name = "Rizalyn Donato Domitita";
-let classmate1Birthdate = "November 13, 2005";
-let classmate1Birthplace = "Southern Ilocos Sur Dist. Hospital- Tagudin, Ilocos Sur, Philippines 2714";
-let classmate1Address = "Al-Aludig, Santiago, Ilocos Sur, Philippines 2707";
+let classmate1Name = "May Flor Pajarillo";
+let classmate1Birthdate = "August 14, 2006";
+let classmate1Birthplace = "Gabriela Silang General Hospital- Vigan City, Ilocos Sur, Philippines 2700";
+let classmate1Address = "Manaboc, Burgos, Ilocos Sur, Philippines 2724";
 let classmate1Course = "Bachelor of Science in Computer Science";
 let classmate1DreamJob = "Software Engineer";
 
@@ -46,11 +45,11 @@ let cmate2birthplace_uppercase = classmate2Birthplace.toUpperCase();
 let cmate2address_uppercase =  classmate2Address.toUpperCase();
 let cmate2course_lowercase = classmate2Course.toLowerCase();
 
-let message1 = '[' +myname_uppercase+ ']' + ' was born ' + '[' +birthdate+ ' at ' + '[' +birthplace_uppercase+ ']' + '.' + ' currently living at ' + '[' +address_uppercase+ ']' + '.' + '[' + myname_lowercase + ']' + ' is taking up ' + '[' + course_lowercase + ']' + ' and dream to be ' + '['+ dreamJob + ']' + 'after graduation.'
-let message2 = '[' +cmate1_uppercase+ ']' + ' was born ' + '[' +classmate1Birthdate+ ' at ' + '[' +cmate1birthplace_uppercase+ ']' + '.' + ' currently living at ' + '[' +cmate1address_uppercase+ ']' + '.' + '[' + cmate1_lowercase + ']' + ' is taking up ' + '[' + cmate1course_lowercase + ']' + ' and dream to be ' + '['+ cmate1course_lowercase + ']' + 'after graduation.'
-let message3 = '[' +cmate2_uppercase+ ']' + ' was born ' + '[' +classmate2Birthdate+ ' at ' + '[' +cmate2birthplace_uppercase+ ']' + '.' + ' currently living at ' + '[' +cmate2address_uppercase+ ']' + '.' + '[' + cmate2_lowercase + ']' + ' is taking up ' + '[' + cmate2course_lowercase + ']' + ' and dream to be ' + '['+ cmate2course_lowercase + ']' + 'after graduation.'
+let message1 = `${myname_uppercase} was born ${birthdate} at ${birthplace_uppercase} and currently living at ${address_uppercase}. ${myname_lowercase} is taking up ${course_lowercase} and dreams to be ${dreamJob} after graduation.`;
+let message2 = `${cmate1_uppercase} was born ${classmate1Birthdate} at ${cmate1birthplace_uppercase} and currently living at ${cmate1address_uppercase}. ${cmate1_lowercase} is taking up ${cmate1course_lowercase} and dreams to be ${classmate1DreamJob} after graduation.`;
+let message3 = `${cmate2_uppercase} was born ${classmate2Birthdate} at ${cmate2birthplace_uppercase} and currently living at ${cmate2address_uppercase}. ${cmate2_lowercase} is taking up ${cmate2course_lowercase} and dreams to be ${classmate2DreamJob} after graduation.`;
 
-// logs the message to the console and print the statement given
+// logs the messages to the console
 console.log(message1);
 console.log(message2);
 console.log(message3);
